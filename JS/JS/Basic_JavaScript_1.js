2948f9205a0be505cb0e927f4c2b980fc7503802
Aurora S <64712260+asiegler1988@users.noreply.github.com>
2020-05-25 16:34:13 -0700

    Publishing of JS Step 65 Assignment

---
+++ JS/JS/Basic_JavaScript_1.js
@@ -0,0 +1,10 @@
+var Workers= "Employees", Employee_A="Jane", Employee_B="Bill", Employee_C="Dave", Employee_D="Ellen", Employee_E="Aurora"; //here is an example of multiple variables in one statement
+var Employee_A = Employee_A.fontcolor("red"); //colors assigned to variables!
+var Employee_B = Employee_B.fontcolor("orange");
+var Employee_C = Employee_C.fontcolor("yellow");
+var Employee_C = Employee_D.fontcolor("green");
+var Employee_D = Employee_D.fontcolor("blue");
+var Employee_E = Employee_E.fontcolor("violet"); //end of colors assigned to variables
+document.write(Employee_D); //document.write method in use
+
+window.alert(10+2); //window.alert method in use, along with an expression
