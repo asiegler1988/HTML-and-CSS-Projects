9770311c9a08a27a512307cdcf4f5dc67815032e
Aurora S <64712260+asiegler1988@users.noreply.github.com>
2020-05-25 20:08:51 -0700

    Adding Basic_Javascript_2 and Basic_HTML_2

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,14 @@
+var Family = "The Bills", Dad = "Cliff", Mom = "Summer", Daughter = "Sue", Son = "Robert";
+document.getElementById
+
+function My_First_Function () {
+    var str = "Cliff Bills' favorite color is blue!";
+    var result = str.fontcolor("blue");
+    document.getElementById("Blue_Text").innerHTML = result;
+}
+
+function myFunction () {
+    var sentence = "I am learning";
+    sentence += "a lot from this book!";
+    document.getElementById("Concatenate").innerHTML=sentence;
+}
