67dda2c06c1c49b18d3d514734e204b4615939b2
Aurora S <64712260+asiegler1988@users.noreply.github.com>
2020-05-26 19:55:45 -0700

    Changed Line 21 of Basic_HTML_2 to myFunction
    Changed Line 21 from "myfunction" to "myFunction". Thank you!

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_3.js
@@ -0,0 +1,9 @@
+function Addition_Operation() {
+    var addition = 4+5;
+    document.getElementById("Math").innerHTML="4+5="+addition;
+}
+
+function Subtraction_Operation() {
+    var subtraction = 10-5;
+    document.getElementById("Math").innerHTML="10-5="+subtraction;
+}
